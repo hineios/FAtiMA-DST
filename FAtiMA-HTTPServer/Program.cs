84b77ddee906975514d07a0e4e74395256456a41
Fábio Vieira Almeida <fabio.vieira.almeida@tecnico.ulisboa.pt>
2017-03-31 17:13:08 +0100

    initial stuff

---
+++ FAtiMA-HTTPServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using AssetManagerPackage;
+using RolePlayCharacter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace FAtiMA_HTTPServer
+{
+    class Program
+    {
+        private static RolePlayCharacterAsset Walter;
+        static void Main(string[] args)
+        {
+            AssetManager.Instance.Bridge = new BasicIOBridge();
+            Console.Write("Loading Character from file... ");
+            Walter = RolePlayCharacterAsset.LoadFromFile("./walter.rpc");
+            Walter.LoadAssociatedAssets();
+            Console.WriteLine("Complete!");
+
+            WebServer ws = new WebServer(SendResponse, "http://localhost:8080/");
+            ws.Run();
+            Console.WriteLine("Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            Console.Write("Deliberating... ");
+
+            if (!request.HasEntityBody)
+            {
+                Console.WriteLine("No new perceptions... ");
+                return null;
+            }
+            using (System.IO.Stream body = request.InputStream) // here we have data
+            {
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(body, request.ContentEncoding))
+                {
+                    Console.WriteLine(" Updating perceptions... ");
+                    return "";
+                }
+            }
+        }
+    }
+}
